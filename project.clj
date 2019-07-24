0af4dff2c85d0c4cd72e78ee91e57dd0ba52add9
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2019-07-24 13:25:39 +0300

    Bump dependencies
    Works with PG 10+
    Works with JDK12
    
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- project.clj
+++ project.clj
@@ -2,18 +2,18 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [postgresql/postgresql "9.3-1102.jdbc41"]
-                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.postgresql/postgresql "42.2.6"]
+                 [org.clojure/java.jdbc "0.7.9"]
                  [lib-noir "0.9.9"]
                  [clj-time "0.15.1"]
-                 [migratus "1.2.0"]
+                 [migratus "1.2.4"]
                  [prone "1.6.1"]
                  [ring/ring-anti-forgery "1.3.0"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.9.2"]
+  :plugins [[lein-ring "0.12.5"]
             [lein-ancient "0.6.15"]
             [migratus-lein "0.5.0"]]
   :migratus {:store :database
