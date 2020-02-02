c9a7247dea960abb6ad870aacb6e2d160e5603e1
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2020-02-02 22:57:10 +0200

    Refactor auth code
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- src/sample/helpers.clj
+++ src/sample/helpers.clj
@@ -3,6 +3,7 @@
             [sample.models.user :as user-db]
             [hiccup.form :refer :all]
             [hiccup.element :refer :all]
+            [noir.validation :as vali]
             [noir.session :as session]))
 
 (defn current-user-id []
@@ -11,8 +12,12 @@
 (defn current-user []
   (user-db/get-user-by-id (current-user-id)))
 
+(defn error-item [[error]]
+  [:div.text-danger error])
+
 (defn input-control [type id name & [value required]]
   [:div.form-group
    (list
      (label id name)
+     (vali/on-error (keyword id) error-item)
      (type {:class "form-control" :required required} id value))])
---
+++ src/sample/views/auth.clj
@@ -0,0 +1,30 @@
+(ns sample.views.auth
+  (:require [hiccup.page :refer [html5 include-css]]
+            [hiccup.element :refer :all]
+            [hiccup.form :refer :all]
+            [clj-time.coerce :as c]
+            [clj-time.format :as f]
+            [sample.models.user :as db]
+            [sample.helpers :refer :all]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [noir.session :as session]))
+
+(defn login-page [& [email]]
+  [:div.login-form
+   [:h1 "Login with existing account"]
+   (form-to [:post "/login"]
+            (anti-forgery-field)
+            (input-control text-field "email" "Email" email)
+            (input-control password-field "password" "Password")
+            (submit-button {:class "btn btn-success"} "Login"))])
+
+(defn registration-page [& [name email]]
+  [:div.registration-form
+   [:h1 "Let's create an account"]
+   (form-to [:post "/register"]
+            (anti-forgery-field)
+            (input-control text-field "name" "Name" name)
+            (input-control text-field "email" "Email" email)
+            (input-control password-field "password" "Password")
+            (input-control password-field "password_confirmation" "Repeat password")
+            (submit-button {:class "btn btn-success"} "Create account"))])
